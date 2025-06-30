127b13ba0963015ae236a48a287eb8309e4a1b64
ManuelFreire-rgb <mfreire.bernal@gmail.com>
2025-06-30 19:53:07 -0400

    Actualizacion app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-import "bootstrap";
-import "./style.css";
+function showCardRandom() {
+  let numeros = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"];
+  let simbolos = ["♥", "♠", "♣", "♦"];
+  
+  let numero = numeros[Math.floor(Math.random() * numeros.length)];
+  let simbolo = simbolos[Math.floor(Math.random() * simbolos.length)];
 
+  document.getElementById("number").textContent = numero;
+  document.getElementById("symbolTop").textContent = simbolo;
+  document.getElementById("symbolBottom").textContent = simbolo;
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  if (simbolo === "♥" || simbolo === "♦") {
+    document.getElementById("card").style.color = "red";
+  }  else {
+    document.getElementById("card").style.color = "black";
+  }
+}
