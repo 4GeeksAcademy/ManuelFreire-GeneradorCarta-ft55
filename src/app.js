ff6271b9a27aac38a31e9a514b99ddd6c92acd66
ManuelFreire-rgb <mfreire.bernal@gmail.com>
2025-07-01 02:23:47 +0000

    Proyecto Finalizado

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 function showCardRandom() {
-  let numeros = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"];
+  let numeros = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
   let simbolos = ["♥", "♠", "♣", "♦"];
-  
+
   let numero = numeros[Math.floor(Math.random() * numeros.length)];
   let simbolo = simbolos[Math.floor(Math.random() * simbolos.length)];
 
@@ -9,9 +9,8 @@ function showCardRandom() {
   document.getElementById("symbolTop").textContent = simbolo;
   document.getElementById("symbolBottom").textContent = simbolo;
 
-  if (simbolo === "♥" || simbolo === "♦") {
-    document.getElementById("card").style.color = "red";
-  }  else {
-    document.getElementById("card").style.color = "black";
-  }
+  (simbolo === "♥") || (simbolo === "♦") ? document.getElementById("card").style.color = "red" : document.getElementById("card").style.color = "black";
+}
+window.onload = function reLoad() {
+  return showCardRandom()
 }
